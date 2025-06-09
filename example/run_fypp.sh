f6acb3afaa9a32afe705d2c7b0e98f9fb7068f81
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-06-09 18:58:53 +0900

    Squashed commit of the following:
    commit 38a5ff7c46e1ff6adb52eeabf23263abea220359
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sat Jun 7 07:59:06 2025 +0900
    
        Fix `taylor_series_cos(x)`
        Fix how to call `horner_***`
    
    commit 4f5bbc996ea1e03cc21909f7b6097c76fc00735a
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sat Jun 7 07:20:43 2025 +0900
    
        Fix `taylor_series_cos(x)`
        Fix how to call `horner_***`
    
    commit 7ace8b67b359ad4050f85a806ec58987331ad199
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sat Jun 7 07:01:49 2025 +0900
    
        Squashed commit of the following:
    
        commit 2d8c013ceec8300ff965ddc7120a6dea9223caf1
        Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
        Date:   Sat Jun 7 01:13:19 2025 +0900
    
            Adjust `example`
            Adjust sample graphs (title & key setting)
    
        commit 80ec0b47aa06a90ba48fe1759f90c2c708b77537
        Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
        Date:   Sat Jun 7 00:48:44 2025 +0900
    
            Update `example`
            Add sample graphs

--- example/run_fypp.sh
+++ example/run_fypp.sh
@@ -13,14 +13,28 @@ fypp -F -DWITH_REAL128  demo_sin.fypp demo_sin_real128.f90
 
 
 
-fypp -F -DWITH_REAL32   demo_cos_gnuplot.fypp demo_cos_real32.plt
-fypp -F -DWITH_REAL64   demo_cos_gnuplot.fypp demo_cos_real64.plt
-fypp -F -DWITH_REAL128  demo_cos_gnuplot.fypp demo_cos_real128.plt
+fypp -F -DWITH_REAL32   demo_cos_gnuplot_error.fypp demo_cos_error_real32.plt
+fypp -F -DWITH_REAL64   demo_cos_gnuplot_error.fypp demo_cos_error_real64.plt
+fypp -F -DWITH_REAL128  demo_cos_gnuplot_error.fypp demo_cos_error_real128.plt
 
-fypp -F -DWITH_REAL32   demo_exp_gnuplot.fypp demo_exp_real32.plt
-fypp -F -DWITH_REAL64   demo_exp_gnuplot.fypp demo_exp_real64.plt
-fypp -F -DWITH_REAL128  demo_exp_gnuplot.fypp demo_exp_real128.plt
+fypp -F -DWITH_REAL32   demo_exp_gnuplot_error.fypp demo_exp_error_real32.plt
+fypp -F -DWITH_REAL64   demo_exp_gnuplot_error.fypp demo_exp_error_real64.plt
+fypp -F -DWITH_REAL128  demo_exp_gnuplot_error.fypp demo_exp_error_real128.plt
 
-fypp -F -DWITH_REAL32   demo_sin_gnuplot.fypp demo_sin_real32.plt
-fypp -F -DWITH_REAL64   demo_sin_gnuplot.fypp demo_sin_real64.plt
-fypp -F -DWITH_REAL128  demo_sin_gnuplot.fypp demo_sin_real128.plt
+fypp -F -DWITH_REAL32   demo_sin_gnuplot_error.fypp demo_sin_error_real32.plt
+fypp -F -DWITH_REAL64   demo_sin_gnuplot_error.fypp demo_sin_error_real64.plt
+fypp -F -DWITH_REAL128  demo_sin_gnuplot_error.fypp demo_sin_error_real128.plt
+
+
+
+fypp -F -DWITH_REAL32   demo_cos_gnuplot_series.fypp demo_cos_series_real32.plt
+fypp -F -DWITH_REAL64   demo_cos_gnuplot_series.fypp demo_cos_series_real64.plt
+fypp -F -DWITH_REAL128  demo_cos_gnuplot_series.fypp demo_cos_series_real128.plt
+
+fypp -F -DWITH_REAL32   demo_exp_gnuplot_series.fypp demo_exp_series_real32.plt
+fypp -F -DWITH_REAL64   demo_exp_gnuplot_series.fypp demo_exp_series_real64.plt
+fypp -F -DWITH_REAL128  demo_exp_gnuplot_series.fypp demo_exp_series_real128.plt
+
+fypp -F -DWITH_REAL32   demo_sin_gnuplot_series.fypp demo_sin_series_real32.plt
+fypp -F -DWITH_REAL64   demo_sin_gnuplot_series.fypp demo_sin_series_real64.plt
+fypp -F -DWITH_REAL128  demo_sin_gnuplot_series.fypp demo_sin_series_real128.plt
