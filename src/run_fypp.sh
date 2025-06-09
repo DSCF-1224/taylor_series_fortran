74f91d414e3129ee6d30fc523fd1de3179900fb1
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-06-09 23:12:26 +0900

    Squashed commit of the following:
    commit deb0c7e382100d8d35ae1f5257f11a91e5c806ab
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Mon Jun 9 23:05:23 2025 +0900
    
        Update the `README`
        Add a link to the documentation for this library.
    
    commit 8e87b637e49943622c1a6ad7feb4e690a0002cb2
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Mon Jun 9 22:57:42 2025 +0900
    
        Fix the `README`
    
    commit ad7dd4c51868ba6e458164139766823dd757f88b
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Mon Jun 9 22:50:52 2025 +0900
    
        Update the `README`
    
    commit 2aa5009053a9754092878a42a52d423a19460ff0
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Mon Jun 9 20:44:05 2025 +0900
    
        Update the documentation for `src`
        Add comments; `public` `interface`

--- src/run_fypp.sh
+++ src/run_fypp.sh
@@ -13,3 +13,4 @@ fypp -F -DWITH_REAL32   submodule_sin.fypp submodule_sin_real32.f90
 fypp -F -DWITH_REAL64   submodule_sin.fypp submodule_sin_real64.f90
 fypp -F -DWITH_REAL128  submodule_sin.fypp submodule_sin_real128.f90
 
+fypp -F                 README.fypp        ../README.md
