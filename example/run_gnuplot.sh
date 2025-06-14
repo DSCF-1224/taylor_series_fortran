548bf5e6792835613d64a2dce5e31a5c0a358b44
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-06-14 20:29:32 +0900

    Squashed commit of the following:
    commit 50943f52bed141d45b0f95abfd93aa564034d04f
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sat Jun 14 20:21:11 2025 +0900
    
        Update README
        Add data: `taylor_series_cosh_***`
    
    commit 8ff9d988c080c3049ad6abaced3346aca0f43939
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sat Jun 14 19:18:58 2025 +0900
    
        Add examples: `taylor_series_cosh_*`
    
    commit e0c3cb89dccd0b1f3a44010b01ec490ba3cf9b1b
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sat Jun 14 18:56:04 2025 +0900
    
        Fix how to set `submodule` name
    
    commit 751fe23f7530c91745ccaa979eabaa0657dba595
    Author: DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
    Date:   Sat Jun 14 18:41:26 2025 +0900
    
        Add Implementation: 'taylor_series_cosh_*`

--- example/run_gnuplot.sh
+++ example/run_gnuplot.sh
@@ -3,6 +3,10 @@ gnuplot demo_cos_error_real32.plt
 gnuplot demo_cos_error_real64.plt
 gnuplot demo_cos_error_real128.plt
 
+gnuplot demo_cosh_error_real32.plt
+gnuplot demo_cosh_error_real64.plt
+gnuplot demo_cosh_error_real128.plt
+
 gnuplot demo_exp_error_real32.plt
 gnuplot demo_exp_error_real64.plt
 gnuplot demo_exp_error_real128.plt
@@ -16,6 +20,10 @@ gnuplot demo_sin_error_real128.plt
 gnuplot demo_cos_series_real32.plt
 gnuplot demo_cos_series_real64.plt
 gnuplot demo_cos_series_real128.plt
+
+gnuplot demo_cosh_series_real32.plt
+gnuplot demo_cosh_series_real64.plt
+gnuplot demo_cosh_series_real128.plt
 
 gnuplot demo_exp_series_real32.plt
 gnuplot demo_exp_series_real64.plt
